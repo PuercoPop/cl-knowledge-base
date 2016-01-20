abfa6c77407ea793a98d2a8926ab7734f069d288
Javier Olaechea <pirata@gmail.com>
2016-01-20 11:10:58 -0500

    Remove cl-rethinkdb helper macros

--- src/db.lisp
+++ src/db.lisp
@@ -11,19 +11,4 @@
                                              :host *db-host*
                                              :password nil)))
 
-(defmacro with-db ((socket-name &key (host *host*) (port *port*))
-                   &body body)
-  "Run the BODY with a socket bound to SOCKET-NAME "
-  `(alet ((,socket-name (connect ,host ,port)))
-     (finally (progn
-                ,@body)
-       (disconnect ,socket-name))))
 
-(defmacro with-query ((result query) &body body)
-  "Run the BODY with the result of QUERY bound to RESULT "
-  (with-unique-names (query-result)
-    (with-gensyms (socket-name)
-      `(with-db (,socket-name)
-         (alet ((,query-result (run ,socket-name (r ,query))))
-           (progv '(,result) ,query-result
-               ,@body))))))
